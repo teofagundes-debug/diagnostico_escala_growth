e19310af1e3bd96e692915a29babde6011cf01a1
teofagundes-debug <teofagundes@gmail.com>
2026-07-12 23:23:18 -0300

    feat: refina documento executivo do Método Escala Growth

---
+++ database/migration_v6_refinamento.sql
@@ -0,0 +1,3 @@
+alter table planos_implantacao add column if not exists meta_ieg jsonb default '{}'::jsonb;
+alter table planos_implantacao add column if not exists indicadores jsonb default '[]'::jsonb;
+
